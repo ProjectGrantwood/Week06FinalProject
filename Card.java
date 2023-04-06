44e054222cb5bfed6b35f1cc5586de42673c08bd
ProjectGrantwood <justinphillipturner@gmail.com>
2023-04-06 14:36:25 -0400

    Populated Card class

---
+++ Card.java
@@ -0,0 +1,42 @@
+package Week06FinalProject;
+
+public class Card  {
+    
+    // Fields:
+    
+    private int value;
+    private String name;
+    
+    // Constructor:
+    
+    public Card(String name, int val) {
+        this.name = name;
+        this.value = val;
+    }
+    
+    // Getters:
+    
+    public int getValue() {
+        return this.value;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    // Setters:
+    
+    public void setValue(int val){
+        this.value = val;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    // methods:
+    
+    public void describe() {
+        System.out.println(this.name + ", with a value of " + this.value);
+    }
+}
